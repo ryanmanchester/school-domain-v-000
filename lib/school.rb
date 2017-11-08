b3ef608c565f8ae95d0acb67526d8bacb3014fa7
Ryan Manchester <ryanm17@gmail.com>
2017-11-08 00:11:56 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,7 @@ attr_accessor :roster
 
   def add_student(student_name, grade)
     @roster[grade] = []
-    @roster[grade] << student_name
+    #@roster[grade] << student_name
     @roster.collect do |grade, name|
       @roster[grade] << name
     end
